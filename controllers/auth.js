0dd879caeeb1a713b802fcce35ec5bdeb39353df
SERGIO-G <01sergiogonzalez01@gmail.com>
2024-02-10 01:50:31 -0300

    Test max Age

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,7 +22,7 @@ export async function login(req, res) {
 
     const token = await createToken({ id: user._id })
     console.log(token);
-    res.cookie("credentials", token, { sameSite: "none", secure: true, maxAge: 60 * 60 * 24 });
+    res.cookie("credentials", token, { sameSite: "none", secure: true, maxAge: 1000 * 60 * 60 * 24 });
     // res.setHeader('set-cookie', `credentials=${token}; Path=/; Secure; SameSite=None; Partitioned;`)
     // res.setHeader('set-cookie', cookie.serialize('credentials', token, { secure: true, sameSite: 'none', path: '/', partitioned: true }))
 
